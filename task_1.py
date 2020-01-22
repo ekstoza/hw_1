604068798c49ed23f15e82ce9859363bb34a77bd
Вадим А. Гордеев <gordeev_va@edu.kvantorium-pk.ru>
2020-01-22 18:33:42 +1000

    done

--- task_1.py
+++ task_1.py
@@ -5,7 +5,8 @@ from pyrob.api import *
 
 @task
 def task_1_1():
-    pass
+    move_down(1)
+    move_right(2)
 
 
 if __name__ == '__main__':
--- task_3.py
+++ task_3.py
@@ -5,8 +5,10 @@ from pyrob.api import *
 
 @task
 def task_3_1():
-    pass
-
+    if (wall_is_on_the_right()):
+        pass
+    else:
+        move_right()
 
 if __name__ == '__main__':
     run_tasks()
