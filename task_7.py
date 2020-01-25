836c76670cf986f66837099022f3b1e464af1f01
Вадим А. Гордеев <gordeev_va@edu.kvantorium-pk.ru>
2020-01-25 16:57:43 +1000

    done

--- task_7.py
+++ task_7.py
@@ -5,7 +5,11 @@ from pyrob.api import *
 
 @task
 def task_5_4():
-    pass
+    while(wall_is_on_the_left()):
+        if(not wall_is_beneath()):
+            move_down()
+        elif(wall_is_beneath()):
+            move_right()
 
 
 if __name__ == '__main__':
